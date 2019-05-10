4b63981632657f3b3c621c1fdf704062e131849f
Jarryd Stringfellow <jarryd.stringfellow@gmail.com>
2019-05-10 14:20:50 +1000

    Setup and added to sample to draw simple shape

--- Vector-Design-Tool/src/sample/Main.java
+++ Vector-Design-Tool/src/sample/Main.java
@@ -6,16 +6,68 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+//Extra imports
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.shape.LineTo;
+import javafx.scene.shape.MoveTo;
+import javafx.scene.shape.Path;
+
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
-    }
+    public void start(Stage stage) throws Exception{
+        //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        //primaryStage.setTitle("Hello World");
+        //primaryStage.setScene(new Scene(root, 300, 275));
+        //primaryStage.show();
 
+        //All code in this section taken from tutorial
+        //https://www.tutorialspoint.com/javafx/javafx_2d_shapes.htm?fbclid=IwAR2NeBM_BUsyXa7Amcx132JK6pfjlsyE-jhmuXzcyrSm67szq6Cpn-pvrjs
+        //================================================
+        //Creating a Path
+        Path path = new Path();
+
+        //Moving to the starting point
+        MoveTo moveTo = new MoveTo(108, 71);
+
+        //Creating 1st line
+        LineTo line1 = new LineTo(321, 161);
+
+        //Creating 2nd line
+        LineTo line2 = new LineTo(126,232);
+
+        //Creating 3rd line
+        LineTo line3 = new LineTo(232,52);
+
+        //Creating 4th line
+        LineTo line4 = new LineTo(269, 250);
+
+        //Creating 4th line
+        LineTo line5 = new LineTo(108, 71);
+
+        //Adding all the elements to the path
+        path.getElements().add(moveTo);
+        path.getElements().addAll(line1, line2, line3, line4, line5);
+
+        //Creating a Group object
+        Group root = new Group(path);
+
+        //Creating a scene object
+        Scene scene = new Scene(root, 600, 300);
+
+        //Setting title to the Stage
+        stage.setTitle("Drawing an arc through a path");
+
+        //Adding scene to the stage
+        stage.setScene(scene);
+
+        //Displaying the contents of the stage
+        stage.show();
+        //=================================================
+    }
 
     public static void main(String[] args) {
         launch(args);
