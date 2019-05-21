8ae6f1ad3b793938a92144582c7837f9af6d644d
Sreya Singh <sreya.singh@connect.qut.edu.au>
2019-05-21 12:58:50 +1000

    Added rectangle class

---
+++ Vector-Design-Tool/src/VectorDesignTool/Rectangle.java
@@ -0,0 +1,12 @@
+package VectorDesignTool;
+
+public class Rectangle {
+    private int length;
+    private int width;
+
+    public Rectangle(int length, int width){
+        this.length = length;
+        this.width = width;
+    }
+
+}
--- Vector-Design-Tool/src/VectorDesignTool/Shapes.java
+++
@@ -1,8 +0,0 @@
-package VectorDesignTool;
-
-public interface Shapes {
-    public void line();
-    public void eclipse();
-    public void rectanlge();
-    public void polygon();
-}
