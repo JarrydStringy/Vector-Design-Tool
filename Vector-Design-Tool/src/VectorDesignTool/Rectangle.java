d6cf2e86e48909ac30052117f0703d44b3e084b7
Sreya Singh <sreya.singh@connect.qut.edu.au>
2019-05-21 13:08:42 +1000

    edited rec

--- Vector-Design-Tool/src/VectorDesignTool/Rectangle.java
+++ Vector-Design-Tool/src/VectorDesignTool/Rectangle.java
@@ -3,11 +3,11 @@ package VectorDesignTool;
 public class Rectangle {
     private int length;
     private int width;
+    private String colour;
 
-    public Rectangle(int length, int width){
+    public Rectangle(int length, int width, String colour){
         this.length = length;
         this.width = width;
+        this.colour = colour;
     }
-
-
 }
