2f53750bd5667ca22a7561310296aa3370667e16
Azeez <azeezbodija@gmail.com>
2019-05-28 12:50:21 +1000

    variables

--- Vector-Design-Tool/src/VectorDesignTool/Controller.java
+++ Vector-Design-Tool/src/VectorDesignTool/Controller.java
@@ -146,6 +146,10 @@ public class Controller {
      */
     public void draw(){
 
+        // Set double variables for canvas width and height
+        double a = canvas.getWidth();
+        double b = canvas.getHeight();
+
         // Set Pen and Fill Colour
         fill.setOnAction(click -> pen.setSelected(!fill.isSelected()));
 
