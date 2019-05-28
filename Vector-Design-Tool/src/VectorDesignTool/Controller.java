2981d66a548464ea461af6ae3eb662061a3f4a0f
Azeez <azeezbodija@gmail.com>
2019-05-28 18:35:28 +1000

    Resize

--- Vector-Design-Tool/src/VectorDesignTool/Controller.java
+++ Vector-Design-Tool/src/VectorDesignTool/Controller.java
@@ -56,6 +56,23 @@ public class Controller {
      * Initialize the application and attach listener to canvas for all methods to draw
      */
     public void initialize() {
+
+        // Readjust canvas values
+        canvasPane.prefWidthProperty().addListener((ov, oldValue, newValue) -> {
+            canvas.setWidth(newValue.doubleValue());
+        });
+        canvasPane.prefHeightProperty().addListener((ov, oldValue, newValue) -> {
+            canvas.setHeight(newValue.doubleValue());
+        });
+        canvasPane.prefWidthProperty().addListener((ov, oldValue, newValue) -> {
+            canvas2.setWidth(newValue.doubleValue());
+        });
+        canvasPane.prefHeightProperty().addListener((ov, oldValue, newValue) -> {
+            canvas2.setHeight(newValue.doubleValue());
+        });
+
+
+
         // Sets graphics context for drawing
         g = canvas.getGraphicsContext2D();
 
