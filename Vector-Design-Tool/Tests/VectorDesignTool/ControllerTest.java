cec2de21a6ede27067da0bcc510007bd1b229e2e
Azeez <azeezbodija@gmail.com>
2019-06-02 18:21:15 +1000

    Junit Files along with Test 4

--- Vector-Design-Tool/Tests/VectorDesignTool/ControllerTest.java
+++ Vector-Design-Tool/Tests/VectorDesignTool/ControllerTest.java
@@ -38,28 +38,41 @@ public class ControllerTest {
     }
 
     /*
-     * Test 1: Choosing the Brush size - must be an integer.
+     * Test 2: Testing the canvas history being cleared
      */
     @Test
-    public void testSaveVECFile() {
+    public void testClearCanvasHistory() {
         controller = new Controller();
 
-//
-//        // Read an input value
-//        String input = controller.inputBrushValue("One");
-//
-//        // Use a boolean to determine if the input is a number or not
-//        boolean value = input.matches("[0-9]*");
-//        boolean value2 = ("9").matches("[0-9]*");
+    }
 
-//        // Test for both a numerical input, character input or string input
-//        assertEquals(false, value);
-//        assertEquals(true, value2);
+    /*
+     * Test 3: Testing the shapes being drawn
+     */
+    @Test
+    public void testShapes() {
+        controller = new Controller();
+
 
 
     }
 
+    /*
+     * Test 4: An error message is created when saving an empty screen.
+     *
+     */
+    @Test
+    public void testEmptySaves() {
+        controller = new Controller();
 
+        // Input a value for a testing number of changes made.
+        int numberofEdits = 0;
 
+        // Use Controller value for number of edits
+        long EditsOnFile = controller.savefile.toString().chars().filter(line -> line == '\n').count();
+
+        // Test if more than zero changes have been mad
+        assertTrue(numberofEdits > EditsOnFile,"Test Edits Variable (" + numberofEdits + ") should be greater than  Edits Made (" + EditsOnFile + ") in order to save the file.");
+    }
 
 }
